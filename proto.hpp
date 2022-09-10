03f184ea1c568fedf533553d2e255ed9bcd0a9f9
NicholasTemesvary <89406929+NicholasTemesvary@users.noreply.github.com>
2022-09-09 21:01:20 -0700

    Create proto.hpp

---
+++ proto.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#ifndef PROTO_H    
+#define PROTO_H     
+#pragma once
+
+template <typename T>
+
+class vector131{
+    private:
+        int size_Array;
+        T *vec
+
+        void push_back;
+        void push_front;
+        int resize;
+
+        void print;
+    public:
+        vector131() {size; vector = }
+
+};
+
+#endif
+
