98d125c8b23d07f8a9393ccf59ca61be54271711
NicholasTemesvary <89406929+NicholasTemesvary@users.noreply.github.com>
2022-09-09 20:59:02 -0700

    Create implemen.cpp

---
+++ implemen.cpp
@@ -0,0 +1,37 @@
+#include "proto.hpp"
+#include <string>
+
+void Vector131::push_back (int size, int num){
+    vec[size] = num;
+    size++;
+}
+
+void Vector131::push_front (int num, int siz)  
+{
+    if (num == capacity)
+    {
+        std::push_front(num);
+    }
+    else {
+        vec[siz] = num;
+    }
+}
+
+int Vector131::resize(int size_declaration)
+{
+    std::cout << "what is the size of array?" << endl; 
+    std::cin >> size_declaration;
+
+    resize(size_declaration);      
+
+    return size_declaration;
+}
+
+
+void Vector131::print()   
+{
+    for (int i = 0;i < size; i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+}
